d859cda0ac6436ede630b93117e869a73ed92eac
Sarah <alahmadisr@gmail.com>
2020-06-13 23:25:29 +0300

    added functionality to shelf component

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -1,48 +1,50 @@
 import React, { Component } from 'react'
 import Shelf from './Shelf'
 export default class BookShelves extends Component {
-    state = {
-        books: [],
-        list: []
-    }
 
+    state = {
+        currentlyReading: [],
+        wantToRead: [],
+        read: []
+    }
 
     componentDidMount() {
-        this.setState({
-            books: this.props.books
-        })
-
-        // this.setState({
-        //     list: filteredList
-        // })
-    }
-    render() {
         var { books } = this.props
 
         if (books) {
-            // console.log(books)
-            const filteredList = books.filter((book) => (
+            console.log(books)
+            const currentlyReading = books.filter((book) => (
                 book.shelf === "currentlyReading"
             ))
+
+            const read = books.filter((book) => (
+                book.shelf === "read"
+            ))
+
+            const wantToRead = books.filter((book) => (
+                book.shelf === "wantToRead"
+            ))
+
+            this.setState({
+                currentlyReading,
+                read,
+                wantToRead
+            })
             console.log('filtering ..')
             console.log('done!')
-            console.log(filteredList)
+            console.log(currentlyReading)
         }
 
+    }
+    render() {
 
-        var test = ''
         return (
 
             <div className="list-books-content">
-                {/* {test = books.filter(book => {
-                    console.log(book.shelf === "read")
-                })} */}
-                {console.log('original list')}
-                {console.log(this.state.books)}
-                {console.log('filtered list')}
-                {console.log(test)}
                 <div>
-                    <Shelf />
+                    <Shelf shelfName='Currently Reading' bookList={this.state.currentlyReading} />
+                    <Shelf shelfName='Want to Read' bookList={this.state.wantToRead} />
+                    <Shelf shelfName='Read' bookList={this.state.read} />
                 </div>
             </div>
         )
--- src/Shelf.js
+++ src/Shelf.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import Book from './Book'
 export default class Shelf extends Component {
     render() {
+        const {shelfName}=this.props
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
+                <h2 className="bookshelf-title">{shelfName}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         <li>
